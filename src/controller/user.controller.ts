6c266a234452aa730cdbf85e11469aceb07a846b
Ibrahim Khawar <ikb1703@gmail.com>
2022-01-31 22:51:09 -0500

    Customer creation

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,7 +1,22 @@
-import { CustomRequest } from "../types";
+import { CustomRequest, Errors } from "../types";
 import { Response } from "express";
 import { User } from "../db/entity/User";
 import { connection } from "../db/connection";
+import { validateEmail } from "../helpers/emailValidation";
+import { v4 as uuid } from "uuid";
+import { SentMessageInfo } from "nodemailer/lib/smtp-transport";
+import * as bcrypt from "bcryptjs";
+import * as nodemailer from "nodemailer";
+
+// Initialize SMTP transporter with nodemailer
+const transporter: nodemailer.Transporter<SentMessageInfo> =
+	nodemailer.createTransport({
+		service: "gmail",
+		auth: {
+			user: process.env.email,
+			pass: process.env.password,
+		},
+	});
 
 export const get_user_data = async (
 	req: CustomRequest<{}, {}, {}>,
@@ -17,4 +32,68 @@ export const get_user_data = async (
 		.getOne();
 
 	res.json(user);
+};
+
+interface createUserBody {
+	userEmail: string;
+	userProject: string;
+}
+
+export const create_user = async (
+	req: CustomRequest<{}, createUserBody, {}>,
+	res: Response
+) => {
+	const { userEmail, userProject } = req.body;
+	const errors: Errors = [];
+
+	if (!userEmail) {
+		errors.push({
+			message: "Please enter an email",
+			location: "emailInput",
+		});
+
+		return res.json(errors);
+	}
+
+	if (!validateEmail(userEmail)) {
+		errors.push({
+			message: "Please enter a valid email",
+			location: "emailInput",
+		});
+
+		return res.json(errors);
+	}
+
+	const foundUser = await connection
+		.getRepository(User)
+		.createQueryBuilder("user")
+		.where("user.userEmail = :userEmail", { userEmail });
+
+	if (foundUser) {
+		errors.push({
+			message: "An account with this email already exists",
+			location: "emailInput",
+		});
+
+		return res.json(errors);
+	}
+
+	const userPassword: string = uuid;
+	const salt = await bcrypt.genSalt(10);
+	const encryptedPassword = await bcrypt.hash(userPassword, salt);
+
+	const newCustomer = new User();
+	newCustomer.userEmail = userEmail;
+	newCustomer.userPassword = encryptedPassword;
+	newCustomer.userProject = userProject;
+	newCustomer.userType = "CUSTOMER";
+
+	await connection.manager.save(newCustomer);
+
+	await transporter.sendMail({
+		from: process.env.email,
+		to: userEmail,
+		subject: "LogiSense Account Creation",
+		text: `An account was created with this email. Your password is ${userPassword}`,
+	});
 };
--- src/router/user.router.ts
+++ src/router/user.router.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { get_user_data } from "../controller/user.controller";
+import { get_user_data, create_user } from "../controller/user.controller";
 
 const router: Router = Router();
 
 // Get user data
 router.get("/", get_user_data);
+
+// Create user (customer)
+router.post("/", create_user);
 
 export default router;
