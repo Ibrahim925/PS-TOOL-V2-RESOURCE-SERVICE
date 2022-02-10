5dff5d04f359c96c177ffea1e648589566cc6e4c
Ibrahim Khawar <ikb1703@gmail.com>
2022-02-09 19:23:16 -0500

    F

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import * as express from "express";
 import * as cors from "cors";
+import { createConnection } from "typeorm";
 import userRouter from "./router/user.router";
 import projectRouter from "./router/project.router";
 import ruleRouter from "./router/rule.router";
 import authenticateRefreshToken from "./middlewares/validateAccessToken";
 import "dotenv/config";
+import "reflect-metadata";
 
 const app = express();
 
@@ -16,6 +18,18 @@ app.use(authenticateRefreshToken);
 app.use("/user", userRouter);
 app.use("/project", projectRouter);
 app.use("/rule", ruleRouter);
+
+(async () => {
+	await createConnection({
+		type: "mysql",
+		host: "localhost",
+		port: 3306,
+		username: "test",
+		password: "test",
+		database: "test",
+		name: "DB",
+	});
+})();
 
 const PORT = process.env.PORT || 5001;
 
