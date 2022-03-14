13fb2a95457c33f9e93af12b4738788235e7d4ac
Ibrahim Khawar <ikb1703@gmail.com>
2022-03-14 12:41:33 -0400

    Error route

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import userRouter from "./router/user.router";
 import projectRouter from "./router/project.router";
 import ruleRouter from "./router/rule.router";
 import notificationRouter from "./router/notification.router";
+import errorRouter from "./router/error.router";
 import authenticateAccessToken from "./middlewares/authenticateAccessToken";
 import "dotenv/config";
 
@@ -18,6 +19,7 @@ app.use("/user", userRouter);
 app.use("/project", projectRouter);
 app.use("/rule", ruleRouter);
 app.use("/notification", notificationRouter);
+app.use("/error", errorRouter);
 
 const PORT = process.env.PORT || 5001;
 
